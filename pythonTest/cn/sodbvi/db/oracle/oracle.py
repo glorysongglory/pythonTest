2a84dbeed469adac6c2bb61f0e09f67f33a4837c
glorysongglory <317878410@qq.com>
2016-05-10 14:02:19 +0800

    modify

--- pythonTest/cn/sodbvi/db/oracle/oracle.py
+++ pythonTest/cn/sodbvi/db/oracle/oracle.py
@@ -8,12 +8,12 @@ import cx_Oracle
 import os
 
 
-db_conn = cx_Oracle.connect("test/test@127.0.0.1/oragbk")
+db_conn = cx_Oracle.connect("oem/oem@192.168.100.250/ora9")
 curs=db_conn.cursor ()
-sql='select * from ai_wallmoney'
+sql='''select * from ai_user_edm0422 a where a.create_date < to_date('2015-06-01','yyyy-mm-dd')'''
 rr=curs.execute (sql)
 row=curs.fetchone()
 while row:
-    (ID,NAME)=(row[5],row[6])
+    (ID,NAME)=(row[8],row[6])
     row=curs.fetchone ()
     print ID,NAME.decode('gbk')
