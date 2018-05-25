8f5f020e31405dddc478c4cc821a07df6e2a40a5
sodbvi <317878410@qq.com>
2018-05-25 18:25:09 +0800

    jinja2 codegen

--- pythonTest/cn/sodbvi/jinjaEx/001template.py
+++ pythonTest/cn/sodbvi/jinjaEx/001template.py
@@ -4,28 +4,18 @@ import os;
 
 from jinja2 import Environment, PackageLoader
 
+filedict = {'apiaction': 'Action.java', 'entity': '.java', 'mapper': 'Mapper.java', 'service': 'Service.java',
+            'xml': '.xml'}
 env = Environment(loader=PackageLoader('result'))
-arg = {"package": "com.hi.webapp.hipro.keyword1", "module": "keyword1", "name": "proKeyword1", "uri": "/test/keyword1",
-       "des": "关键词表", "table": "PRO_KEYWORD1", "fieldList": [
-        {"name": "id", "column": "ID", "type": "long", "dbtype": "BIGINT", "getset": "N", "startend": "N", "des": "主键"},
-        {"name": "createDate", "column": "CREATE_DATE", "type": "Date", "dbtype": "TIMESTAMP", "getset": "N",
-         "startend": "Y", "des": "创建时间"},
-        {"name": "updateDate", "column": "UPDATE_DATE", "type": "Date", "dbtype": "TIMESTAMP", "getset": "N",
-         "startend": "Y", "des": "更新时间"},
-        {"name": "deleteTag", "column": "DELETE_TAG", "type": "String", "dbtype": "VARCHAR", "getset": "N",
-         "des": "逻辑标志"},
-        {"name": "wallId", "column": "WALL_ID", "type": "long", "dbtype": "BIGINT", "getset": "Y", "des": "所属活动ID"},
-        {"name": "actType", "column": "ACT_TYPE", "type": "String", "dbtype": "VARCHAR", "getset": "Y", "des": "活动类型"},
-        {"name": "keyWord", "column": "KEY_WORD", "type": "String", "dbtype": "VARCHAR", "getset": "Y", "des": "关键词"}]}
-
 workPath = os.path.dirname(os.path.realpath(__file__))
-moduleName = arg['module'];
-modulePath = workPath + '\\result\\output\\' + moduleName + '\\'
 env.trim_blocks = True
 
-def genCode(dirname, fileName):
+
+def genCode(jsonStr, dirname, fileName):
     template = env.get_template(dirname + '.j2')
-    content = template.render(arg)
+    content = template.render(jsonStr)
+    moduleName = jsonStr['module'];
+    modulePath = workPath + '\\result\\output\\' + moduleName + '\\'
     childPath = modulePath + dirname
     filePath = childPath + '\\' + moduleName.capitalize() + fileName
     if (not os.path.exists(childPath)):
@@ -35,7 +25,27 @@ def genCode(dirname, fileName):
     fo.write(content.encode().decode('utf-8'))
     fo.close()
 
-if __name__ == '__main__':
-    filedict = {'apiaction': 'Action.java', 'mapper': 'Mapper.java', 'service': 'Service.java', 'xml': '.xml'}
+
+def pre(jsonStr):
     for key, value in filedict.items():
-        genCode(key, value)
+        genCode(jsonStr, key, value)
+
+
+if __name__ == '__main__':
+    jsonStr = {"package": "com.hi.webapp.hipro.keyword1", "module": "keyword1", "name": "proKeyword1",
+               "uri": "/test/keyword1",
+               "des": "关键词表", "table": "PRO_KEYWORD1", "fieldList": [
+            {"name": "id", "column": "ID", "type": "long", "dbtype": "BIGINT", "getset": "N", "startend": "N",
+             "des": "主键"},
+            {"name": "createDate", "column": "CREATE_DATE", "type": "Date", "dbtype": "TIMESTAMP", "getset": "N",
+             "startend": "Y", "des": "创建时间"},
+            {"name": "updateDate", "column": "UPDATE_DATE", "type": "Date", "dbtype": "TIMESTAMP", "getset": "N",
+             "startend": "Y", "des": "更新时间"},
+            {"name": "deleteTag", "column": "DELETE_TAG", "type": "String", "dbtype": "VARCHAR", "getset": "N",
+             "des": "逻辑标志"},
+            {"name": "wallId", "column": "WALL_ID", "type": "long", "dbtype": "BIGINT", "getset": "Y", "des": "所属活动ID"},
+            {"name": "actType", "column": "ACT_TYPE", "type": "String", "dbtype": "VARCHAR", "getset": "Y",
+             "des": "活动类型"},
+            {"name": "keyWord", "column": "KEY_WORD", "type": "String", "dbtype": "VARCHAR", "getset": "Y",
+             "des": "关键词"}]}
+    pre(jsonStr)
