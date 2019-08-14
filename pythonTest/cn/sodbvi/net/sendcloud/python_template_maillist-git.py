db08b0169e681504c32b5faeb0cf6f954fdd4ddc
sodbvi <317878410@qq.com>
2019-08-14 10:47:39 +0800

    requests phone

--- pythonTest/cn/sodbvi/net/sendcloud/python_template_maillist-git.py
+++ pythonTest/cn/sodbvi/net/sendcloud/python_template_maillist-git.py
@@ -5,17 +5,17 @@ import requests
 url = "http://sendcloud.sohu.com/webapi/mail.send_template.json"
 
 API_USER = 'abc'
-API_KEY = '123'
+API_KEY = 'abc'
 
 params = {
     "api_user": API_USER,  # 使用api_user和api_key进行验证
     "api_key": API_KEY,
-    "to": "123@abc.org",  # 使用地址列表的别称地址
-    "from": "abc@123.com",  # 发信人, 用正确邮件地址替代
-    "fromname": "123",
-    "replyto": "abc",
-    "subject": "123",
-    "template_invoke_name": "sadf",
+    "to": "sitetest@maillist.sendcloud.org",  # 使用地址列表的别称地址
+    "from": "",  # 发信人, 用正确邮件地址替代
+    "fromname": "",
+    "replyto": "",
+    "subject": "",
+    "template_invoke_name": "siteUpdate",
     "use_maillist": "true",
     "resp_email_id": "true",
 }
